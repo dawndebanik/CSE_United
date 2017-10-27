aed3fb92f45fe704fdede0de6202832669ad55b0
Suyash Mittal <suyash8514@gmail.com>
2017-10-28 04:55:11 +0530

    Separated drawer from main layout and added it to login, registration and discuss activity.

---
+++ app/src/main/java/com/cseunited/alumni/cseunited/BaseActivity.java
@@ -0,0 +1,35 @@
+package com.cseunited.alumni.cseunited;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.Window;
+
+/**
+ * Created by Suyash on 10/28/2017
+ */
+
+class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    protected DrawerLayout mDrawer;
+    protected NavigationView navigationView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.drawer_layout);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+}
--- app/src/main/java/com/cseunited/alumni/cseunited/MainActivity.java
+++ app/src/main/java/com/cseunited/alumni/cseunited/MainActivity.java
@@ -1,49 +1,60 @@
 package com.cseunited.alumni.cseunited;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
-    private AppCompatButton loginBtn, forumBtn;
+    private AppCompatButton loginBtn, forumBtn, detailsBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Inflating the layout with the drawer layout
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_main, null, false);
+        mDrawer.addView(contentView, 0);
+
+        //Setting up the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Setting up hamburger icon
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //Selecting the option in navigation view
+        navigationView.getMenu().getItem(0).setChecked(true);
+
         initViews();
         initListeners();
         initObjects();
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void initViews() {
         loginBtn = (AppCompatButton) findViewById(R.id.login_view);
         forumBtn = (AppCompatButton) findViewById(R.id.forum_view);
+        detailsBtn = (AppCompatButton) findViewById(R.id.details_view);
     }
 
     private void initListeners() {
         loginBtn.setOnClickListener(this);
         forumBtn.setOnClickListener(this);
+        detailsBtn.setOnClickListener(this);
     }
 
     private void initObjects() {
@@ -61,13 +72,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.home) {
-
-        } else if (id == R.id.events) {
+        if (id == R.id.events) {
 
         } else if (id == R.id.gallery) {
 
@@ -90,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             Intent intent = new Intent(this, DiscussActivity.class);
             startActivity(intent);
         }
-        else if(id == R.id.details){
+        else if(id == R.id.details_view){
             Intent intent = new Intent(this, UnitedDetails.class);
             startActivity(intent);
         }
