0e9fe21adb0eb2e077750baa55070d6d0cba3ec7
Suyash Mittal <suyash8514@gmail.com>
2017-10-28 21:32:08 +0530

    Minor Changes

--- app/src/main/java/com/cseunited/alumni/cseunited/QueryPostActivity.java
+++ app/src/main/java/com/cseunited/alumni/cseunited/QueryPostActivity.java
@@ -1,7 +1,6 @@
 package com.cseunited.alumni.cseunited;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
@@ -13,15 +12,20 @@ import android.view.View;
 import android.widget.Toast;
 
 /**
- * Created by Sachin on 10/28/2017.
+ * Created by Sachin on 10/28/2017
+ * Edited by Suyash
  */
 
 public class QueryPostActivity extends BaseActivity implements View.OnClickListener {
-    private TextInputEditText textInputEditQustion;
-    private TextInputEditText textInputEditQuery;
-    private AppCompatButton appCompatButtonRegister;
+
+    private TextInputEditText textInputEditTitle;
+    private TextInputEditText textInputEditDesc;
+    private AppCompatButton appCompatButtonPost;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Inflating the drawer layout
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.activity_add_question, null, false);
         mDrawer.addView(contentView, 0);
@@ -34,20 +38,21 @@ public class QueryPostActivity extends BaseActivity implements View.OnClickListe
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawer.addDrawerListener(toggle);
         toggle.syncState();
+
         navigationView.getMenu().getItem(3).setChecked(true);
 
-        textInputEditQustion = (TextInputEditText) findViewById(R.id.QustionTitle);
-        textInputEditQuery = (TextInputEditText) findViewById(R.id.AddQuery);
-        appCompatButtonRegister = (AppCompatButton) findViewById(R.id.PostQuery);
-        appCompatButtonRegister.setOnClickListener(this);
+        textInputEditTitle = (TextInputEditText) findViewById(R.id.new_question_title);
+        textInputEditDesc = (TextInputEditText) findViewById(R.id.new_question_desc);
+        appCompatButtonPost = (AppCompatButton) findViewById(R.id.new_question_btn);
+        appCompatButtonPost.setOnClickListener(this);
     }
 
-
     @Override
-    public void onClick(View view) {
-        Toast.makeText(this,"Thanks for posting",Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, DiscussActivity.class);
-        startActivity(intent);
-
+    public void onClick(View v) {
+        String title = textInputEditTitle.getText().toString();
+        String desc = textInputEditDesc.getText().toString();
+        //Do web thing
+        Toast.makeText(this, "POSTED...", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
