00748f51168ad306d17153a2a4a5a21413199ef5
Debanik <debanik96@gmail.com>
2017-11-23 21:41:28 +0530

    Added second layer of gallery.

--- app/src/main/java/com/cseunited/alumni/cseunited/GalleryDetailActivity.java
+++ app/src/main/java/com/cseunited/alumni/cseunited/GalleryDetailActivity.java
@@ -1,26 +1,22 @@
 package com.cseunited.alumni.cseunited;
-import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.view.View;
-import android.widget.Toast;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class GalleryActivity extends BaseActivity implements GalleryAdapter.ImageClickListener {
+public class GalleryDetailActivity extends AppCompatActivity {
     RecyclerView galleryView;
-    Map<View, String> gallerySelector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
         initGalleryView();
-        gallerySelector = new HashMap<>();
     }
     private void initGalleryView(){
         setContentView(R.layout.activity_gallery);
@@ -43,13 +39,7 @@ public class GalleryActivity extends BaseActivity implements GalleryAdapter.Imag
         for(int i=0;i<120;i++)
             labels.add("DUMMY TEXT");
         GalleryAdapter adapter = new GalleryAdapter(this, urls, labels);
-        adapter.setImageClickListener(this);
         galleryView.setAdapter(adapter);
         galleryView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
-    }
-    @Override
-    public void onImageClick(View view) {
-        //Toast.makeText(getApplicationContext(), "Click!", Toast.LENGTH_LONG).show();
-        startActivity(new Intent(this, GalleryDetailActivity.class));
     }
 }
